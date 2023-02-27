6307b0bdb34d5056e3a6888bad30f4ed26de1a48
Shinnken <konrad.suchodolski50@gmail.com>
2023-02-27 16:58:51 +0100

    armkinematic

--- TestArmKinematic.py
+++ TestArmKinematic.py
@@ -17,9 +17,29 @@ armjoint[3] = Servo(10)
 
 arm_lenght = 100
 
-#first servo angle is based on hand-marker rotation
+def rad_to_deg(rad):
+    return rad * 180 / np.pi
+
+#first servo angle is based on hand-marker rotation <- rot[x][1]
 #second servo angle is based on z-axis location of hand-marker
 #third servo angle is based on hand-marker distance from shoulder-marker
+#fourth servo angle is based on hand-marker rotation <- rot[x][0]
+
+#get first servo angle
+def get_servo1_angle(servo ,rot):
+    return rad_to_deg(rot[servo][1])
+
+def get_servo2_angle(servo ,dist):
+
+def get_servo3_angle(servo ,dist):
+    return 1
+
+#calculate angle in triangle having 3 sides
+def get_angle(a,b,c):
+    return np.arccos((a**2 + b**2 - c**2)/(2*a*b))
+
+
+
 
 
 
