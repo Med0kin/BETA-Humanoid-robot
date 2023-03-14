bc6b3f81ac279530554819f9a4677009972b7f53
Shinnken <konrad.suchodolski50@gmail.com>
2023-03-14 06:41:29 +0100

    more testing

--- Servos/Servo.py
+++ Servos/Servo.py
@@ -87,6 +87,7 @@ if __name__ == "__main__":
     test = Servo()
     test.set(2, 20)
     test.set(5, 20)
+    time.sleep(2)
     test.callback()
     # for i in range(10):
     #     test.set(i, 0)
