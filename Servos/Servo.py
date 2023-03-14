4c6b604aa648ca981c5158042b671bafc41d0365
Shinnken <konrad.suchodolski50@gmail.com>
2023-03-14 05:51:21 +0100

    next test

--- Servos/Servo.py
+++ Servos/Servo.py
@@ -11,8 +11,8 @@ class Servo:
         ports = serial_ports()
         if len(ports) == 0:
             raise Exception("No serial ports found")
-        self.__digital = OCServo(ports[0])
-        self.__armjoint = [AServo]*10
+        self._digital = OCServo(ports[0])
+        self._armjoint = [AServo]*10
 
         self.gpio = AServo()
 
@@ -21,25 +21,25 @@ class Servo:
         #                    0  1  2  3   8  9    4  5  6  7
 
         # Create the servo objects
-        self.__armjoint[0] = AServo(17)
-        self.__armjoint[1] = AServo(27)
-        self.__armjoint[2] = AServo(22)
-        self.__armjoint[3] = AServo(10)
+        self._armjoint[0] = AServo(17)
+        self._armjoint[1] = AServo(27)
+        self._armjoint[2] = AServo(22)
+        self._armjoint[3] = AServo(10)
 
-        self.__armjoint[4] = AServo(13)
-        self.__armjoint[5] = AServo(19)
-        self.__armjoint[6] = AServo(26)
-        self.__armjoint[7] = AServo(21)
-        self.__armjoint[8] = AServo(9)      # hip 0
-        self.__armjoint[9] = AServo(11)     # hip 1
+        self._armjoint[4] = AServo(13)
+        self._armjoint[5] = AServo(19)
+        self._armjoint[6] = AServo(26)
+        self._armjoint[7] = AServo(21)
+        self._armjoint[8] = AServo(9)      # hip 0
+        self._armjoint[9] = AServo(11)     # hip 1
         servo180 = [2, 3, 4, 5]
         servo270 = [0, 1, 6, 7]
 
         for i in servo180:
-            self.__armjoint[i].set_range(180)
+            self._armjoint[i].set_range(180)
 
         for i in servo270:
-            self.__armjoint[i].set_range(270)
+            self._armjoint[i].set_range(270)
 
     def callback(self):
         self.gpio.kill()
@@ -48,17 +48,17 @@ class Servo:
         if servo < 0 or servo > 17:
             raise Exception("Servo number out of range")
         elif servo < 10:
-            self.__armjoint[servo].move_servo(angle)
+            self._armjoint[servo].move_servo(angle)
         else:
-            self.__digital.send(servo, angle)
+            self._digital.send(servo, angle)
 
     def get(self, servo):
         if servo < 0 or servo > 17:
             raise Exception("Servo number out of range")
         elif servo < 10:
-            return self.__armjoint[servo].get_angle()
+            return self._armjoint[servo].get_angle()
         else:
-            return self.__digital.get(servo)
+            return self._digital.get(servo)
 
 
 if __name__ == "__main__":
