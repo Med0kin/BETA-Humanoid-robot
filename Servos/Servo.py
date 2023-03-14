e1c9ca79cfd1b6630f2b6d44169b8b5203befb05
Shinnken <konrad.suchodolski50@gmail.com>
2023-03-14 20:52:24 +0100

    Time patch

--- Servos/Servo.py
+++ Servos/Servo.py
@@ -130,7 +130,7 @@ if __name__ == "__main__":
     akcja = ['p13', 'rgdL1', 'rgdL2', 'rgdL1', 'rgdL2', 'rrR1', 'rrR2','rrR1','rrR2','fRL1','fRL2','fRL3','fRL4','fRL1','fRL2','fRL3','fRL4','pr1','pr','ch12','j22','default','pb1','default','pbn1','p13','pr1','pr']
     test = Servo()
     #test.set_many_digital(servolist, anglelist)
-    test.setsequence(akcja,10)
+    test.setsequence(akcja, 5)
 
 
     time.sleep(5)
