ba09a4437de4384a4e800f6e514e8e75ee64f89d
Shinnken <konrad.suchodolski50@gmail.com>
2023-04-01 21:21:07 +0200

    :construction: Server testing

--- tests/client.py
+++ tests/client.py
@@ -1,33 +1,31 @@
-import cv2
-import numpy as np
-import socket
-import struct
+# import libraries
+from vidgear.gears import VideoGear
+from vidgear.gears import NetGear
 
-# Set up socket connection
-HOST = '192.168.1.10'  # IP address of receiving computer
-PORT = 8080
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((HOST, PORT))
+stream = VideoGear(camera_num=0,stream_mode=True).start()  # Open any video stream
+server = NetGear()  # Define netgear server with default settings
 
-# Send video stream
-cap = cv2.VideoCapture(0)
+# infinite loop until [Ctrl+C] is pressed
 while True:
-    # Capture frame
-    ret, frame = cap.read()
-    # Encode frame
-    encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-    result, encimg = cv2.imencode('.jpg', frame, encode_param)
-    # Debugging statements
-    print(f"Encoded image size: {encimg.size}")
-    # Send encoded frame
-    data = np.array(encimg)
-    stringData = data.tostring()
-    sock.sendall(struct.pack('!I', len(stringData)))
-    sock.sendall(stringData)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    try:
+        frame = stream.read()
+        # read frames
+
+        # check if frame is None
+        if frame is None:
+            # if True break the infinite loop
+            break
+
+        # do something with frame here
+
+        # send frame to server
+        server.send(frame)
+
+    except KeyboardInterrupt:
+        # break the infinite loop
         break
 
-# Clean up
-cap.release()
-cv2.destroyAllWindows()
-sock.close()
+# safely close video stream
+stream.stop()
+# safely close server
+writer.close()
