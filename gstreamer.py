82a9ccb6a625af57577445d2a0480bcf39b42b7e
Shinnken <konrad.suchodolski50@gmail.com>
2023-05-11 21:14:56 +0200

    :construction: Testing

--- gstreamer.py
+++ gstreamer.py
@@ -551,7 +551,6 @@ class GstPipeline:
                 if not self.running:
                     break
                 output = self.output
-                print('Render thread: ', output)
                 self.output = None
 
             svg, freeze = self.render_callback(output, self.src_size, self.get_box())
