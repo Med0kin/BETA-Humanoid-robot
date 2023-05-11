3dd2542649021018b38d97b000cabe19d3fbbdad
Shinnken <konrad.suchodolski50@gmail.com>
2023-05-11 22:04:03 +0200

    :construction: Testing

---
+++ pose_camera3.py
@@ -0,0 +1,34 @@
+from tflite_runtime.interpreter import Interpreter
+import os
+import cv2
+import numpy as np
+from PIL import Image
+from PIL import ImageDraw
+from pose_engine import PoseEngine
+import time
+
+engine = PoseEngine('models/posenet_mobilenet_v1_075_481_641_quant_decoder_edgetpu.tflite')
+video = cv2.VideoCapture("/dev/video0")
+time.sleep(1)
+while True:
+    ret, frame = video.read()
+    if not ret:
+        print("empty frame")
+        break
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = cv2.resize(cv2.flip(frame, 1), (640, 480))
+    jpg = Image.fromarray(frame).convert('RGB')
+
+    poses, _ = engine.DetectPosesInImage(jpg)
+    for pose in poses:
+        print('\nPose Score: ', pose.score)
+        for label, keypoint in pose.keypoints.items():
+            print(' %-20s x=%-4d y=%-4d score=%.1f' %
+                  (label.name, keypoint.point[0], keypoint.point[1], keypoint.score))
+            
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+video.release()
+cv2.destroyAllWindows()
