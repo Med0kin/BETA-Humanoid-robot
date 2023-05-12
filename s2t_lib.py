5795d92ea7aec4b3420a01b30e812a1c4822709f
Shinnken <konrad.suchodolski50@gmail.com>
2023-05-12 07:41:32 +0200

    :construction: Test

--- s2t_lib.py
+++ s2t_lib.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import Select
+from webdriver_manager.chrome import ChromeDriverManager
 import threading
 import time
 
@@ -10,14 +11,17 @@ class speech_to_text():
     def __init__(self):
         browserDIR = "--user-data-dir=/home/pi/.config/chromium/Default"
         webdriverDIR = "/usr/lib/chromium-browser/chromedriver"
+        chromium_version = "92.0.4515.98"
         # C:\Users\Shinken\AppData\Local\Google\Chrome\User Data\Default
         options = Options()
         options.add_argument(browserDIR)
         options.add_experimental_option("prefs", {
             "profile.default_content_setting_values.media_stream_mic": 1,
         })
-        service = Service(webdriverDIR)
-        self.stt = webdriver.Chrome(webdriverDIR, options=options)
+        service = Service(ChromeDriverManager(version=chromium_version).install())
+        self.stt = webdriver.Chrome(ChromeDriverManager(version=chromium_version).install(), options=options)
+        # service = Service(webdriverDIR)
+        # self.stt = webdriver.Chrome(webdriverDIR, options=options)
         self.stt.get('https://smodin.io/pl/przemowienie-do-tekst-i-tekst-do-przemowienie')
         self.stt.minimize_window()
         self.stt.find_element("xpath", "//button[normalize-space()='Mowa na tekst']").click()
