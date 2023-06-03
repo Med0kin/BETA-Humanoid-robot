86a95677371c29bc0623dc5dd8a3cf307d3942f3
Med0kin <nk.kulis@gmail.com>
2023-06-03 15:25:02 +0200

    :construction: Add changes

--- s2t_lib.py
+++ s2t_lib.py
@@ -50,7 +50,7 @@ class speech_to_text():
         self.get_text_thread_running = False
         self.get_text_thread.join()
 
-    def change_language(self, language: str) -> None:
+    def set_language(self, language: str) -> None:
         self.stt.find_element("xpath", "//div[@class='styles_navEnd__ys5VT']//button[1]//span[1]").click()
         if(language == "us"):
             self.stt.find_element("xpath", "//li[contains(@value,'en')]//span[contains(@class,'styles_rippleSpan__M7thf')]").click()
