7ec173814d776a51950de42620ddad297c5e71e9
Med0kin <nk.kulis@gmail.com>
2023-06-03 16:46:51 +0200

    :construction: Test

--- s2t_lib.py
+++ s2t_lib.py
@@ -25,13 +25,14 @@ class speech_to_text():
         self.stt = webdriver.Chrome(webdriverDIR, options=options)
         self.stt.get('https://smodin.io/pl/przemowienie-do-tekst-i-tekst-do-przemowienie')
         self.stt.minimize_window()
-        self.stt.find_element("xpath", "//button[normalize-space()='Mowa na tekst']").click()
+        self.stt.find_element("xpath", "//button[@value='Speech']").click()
         time.sleep(2)
         self.stt.find_element("xpath", "//body/div[@id='__next']/div[3]/div[1]/div[1]/div[2]/div[2]/button[1]/span[1]").click()
         time.sleep(2)
         self.textbox = self.stt.find_element("xpath", "//textarea[contains(@placeholder,'Naciśnij przycisk i zacznij mówić')]")
         self.clear = self.stt.find_element("xpath", "//span[@data-text='Kasować']//button//span").click
-        self.get_text_thread_running = True
+        self.pause = False
+        self.get_text_thread_running = False
         self.get_text_thread = threading.Thread(target=self.get_text)
         self.get_text_thread.start()
         self.s2t_text = ''
@@ -42,21 +43,49 @@ class speech_to_text():
                 print(self.textbox.text)
                 self.s2t_text = self.textbox.text
                 self.clear()
-            if not self.get_text_thread_running:
+            if not self.get_text_thread_running :
                 print("get_text_thread stoped")
                 break
+            while self.pause:
+                time.sleep(0.5)
+
     def close_thread(self):
         self.stt.quit()
         self.get_text_thread_running = False
         self.get_text_thread.join()
 
     def set_language(self, language: str) -> None:
+        self.pause = True
         self.stt.find_element("xpath", "//button[contains(@class,'styles_translationBtn__iYTkV')]").click()
         if(language == "us"):
-            self.stt.find_element("xpath", "//li[contains(@value,'en')]//span[contains(@class,'styles_rippleSpan__M7thf')]").click()
+            self.stt.get('https://smodin.io/speech-to-text-and-text-to-speech')
+            self.stt.minimize_window()
+            self.stt.find_element("xpath", "//button[@value='Speech']").click()
+            time.sleep(2)
+            self.stt.find_element("xpath", "//body/div[@id='__next']/div/div/div/div/div/button/span[1]").click()
+            time.sleep(2)
+            self.textbox = self.stt.find_element("xpath", "//textarea[contains(@placeholder,'Press the Button And Start Speaking')]")
+            self.clear = self.stt.find_element("xpath", "//span[@data-text='Delete']//button//span").click
+            self.pause = False
         elif(language == "pl"):
-            self.stt.find_element("xpath", "//li[contains(@value,'pl')]//span[contains(@class,'styles_rippleSpan__M7thf')]").click()
+            self.stt.get('https://smodin.io/pl/przemowienie-do-tekst-i-tekst-do-przemowienie')
+            self.stt.minimize_window()
+            self.stt.find_element("xpath", "//button[@value='Speech']").click()
+            time.sleep(2)
+            self.stt.find_element("xpath", "//body/div[@id='__next']/div[3]/div[1]/div[1]/div[2]/div[2]/button[1]/span[1]").click()
+            time.sleep(2)
+            self.textbox = self.stt.find_element("xpath", "//textarea[contains(@placeholder,'Naciśnij przycisk i zacznij mówić')]")
+            self.clear = self.stt.find_element("xpath", "//span[@data-text='Kasować']//button//span").click
+            self.pause = False
         elif(language == "cs"):
-            self.stt.find_element("xpath", "//li[@value='cs']//span").click()
+            self.stt.get('https://smodin.io/cs/mluveny-projev-na-text-a-text-na-mluveny-projev')
+            self.stt.minimize_window()
+            self.stt.find_element("xpath", "//button[@value='Speech']").click()
+            time.sleep(2)
+            self.stt.find_element("xpath", "//body/div[@id='__next']/div/div/div/div/div/button/span[1]`").click()
+            time.sleep(2)
+            self.textbox = self.stt.find_element("xpath", "//textarea[@placeholder='Stiskněte tlačítko a začněte mluvit']")
+            self.clear = self.stt.find_element("xpath", "//span[@data-text='Vymazat']//button//span").click
+            self.pause = False
         else:
             raise Exception("Language not supported")
